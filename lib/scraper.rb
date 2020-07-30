1e9e7f31200494d6b8bec29304504c5b1a2740b9
Ian Mayton <ian.mayton@gmail.com>
2020-07-30 16:08:13 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -29,9 +29,9 @@ class Scraper
   def make_courses
     get_courses.each do |course| 
       new_course = Course.new
-      new_course.title = course.css('.h2')
-      new_course.schedule = course.css('.date')
-      new_course.description = course.css('.p')
+      new_course.title = course.css('.h2').text
+      new_course.schedule = course.css('.date').text
+      new_course.description = course.css('.p').text
     end
   end 
   
